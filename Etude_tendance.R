9af4789ae2f6446c97ba737f4be9a55121718720
Kaamelotte <caillebotte.antoine@gmail.com>
2021-02-17 21:43:22 +0100

    RAjout des fonctions d'étude de tendance, Gaussien, MoyMbile, polynome locaux et de regression

---
+++ Etude_tendance.R
@@ -0,0 +1,106 @@
+setwd("C:\Users\caill\Documents\Projet")
+rm(list=objects())
+
+library(tidyverse)
+library(lubridate)
+library(xts)
+
+data_train <- read_delim(file = "data/train.csv", delim=",")
+data_test <- read_delim(file = "data/test.csv", delim=",")
+
+
+names(data_train)
+range(data_train$Date)
+
+n = length(Date)
+t = c(1:n)
+
+Tt = t/20 + 1
+omega = 2*pi/50
+St = cos(omega*t) + sin(omega*t)
+noise = rnorm(n,0,1)
+
+Xt = Tt + St + noise
+Yt = Tt + St
+
+Xt = xts(Xt, order.by = Date)
+Yt = xts(Yt, order.by = Date)
+Tt = xts(Tt, order.by = Date)
+
+#_______Affichage________#
+png(file = "Exo1_plot.png", width=600, height=350)
+plot(Xt, type = "l", col = "purple", xlab = "Annee")
+lines(Yt, type = "l", col = "green", lwd=2)
+lines(Tt, type = "l", col = "red", lwd=2)
+
+addLegend(legend.loc = "topleft", legend.names = c("Xt", 'Yt', 'Tt'),
+          col = c("purple","green","red"),lty = c(1, 1) )
+dev.off()
+
+#######################################################################################
+#__________________________________Regression_________________________________________#
+#######################################################################################
+reglin <- lm(Xt ~ t)
+
+summary(reglin)
+
+LinearModels_trend <- reglin$fitted.values
+#_______Affichage________#
+png(file = "Exo1_Reg.png", width=600, height=350)
+plot(Xt, type='l', col = "purple", xlab = "Annee")
+lines(LinearModels_trend, col='red', lwd=2)
+addLegend(legend.loc = "topleft", legend.names = c("Xt", 'Reg'),
+          col = c("purple","blue"),lty = c(1, 1) )
+dev.off()
+
+#######################################################################################
+#________________________________moyenne mobile ______________________________________#
+#######################################################################################
+MovingAverage_trend = stats::filter(Xt, 
+                                    filter = array(1/10, dim=10),
+                                    method = c('convolution'),
+                                    sides = 2,
+                                    circular = FALSE)
+
+MovingAverage_trend = xts(MovingAverage_trend, order.by = Date)
+#_______Affichage________#
+png(file = "Exo1_MoyMobile.png", width=600, height=350)
+plot(Xt, type='l', col = "purple", xlab = "Annee")
+lines(MovingAverage_trend, col='red', lwd=2)
+addLegend(legend.loc = "topleft", legend.names = c("Xt", 'moyenne mobile'),
+          col = c("purple","red"),lty = c(1, 1)) 
+dev.off()
+
+#######################################################################################
+#________________________________Noyau Gaussien_______________________________________#
+#######################################################################################
+kernel_smooth = function(x){
+  value = (x - t)/h
+  kernel = 1/(2*pi) * exp((-value^2)/2)
+  return(sum(Xt*kernel)/sum(kernel))
+}
+
+h=10
+x = t
+kernel_trend = sapply(x,kernel_smooth)
+#_______Affichage________#
+png(file = "Exo1_Gaussien.png", width=600, height=350)
+plot(Xt, type='l', col = "purple", xlab = "Annee")
+lines(xts(kernel_trend,order.by = Date),col='red', lwd=2)
+addLegend(legend.loc = "topleft", legend.names = c("Xt", 'Noyau Gaussien'),
+          col = c("purple","red"),lty = c(1, 1) )
+dev.off()
+
+#######################################################################################
+#_______________________________Polynômes locaux______________________________________#
+#######################################################################################
+
+lo = loess(Xt ~ t, degree = 2, span = 0.7)
+poly_trend = lo$fitted
+#_______Affichage________#
+png(file = "Exo1_PolLoc.png", width=600, height=350)
+plot(Xt, type='l', col = "purple", xlab = "Annee")
+lines(xts(poly_trend,order.by = Date),col='red', lwd=2)
+addLegend(legend.loc = "topleft", legend.names = c("Xt", 'Polynômes locaux'),
+          col = c("purple","red"),lty = c(1, 1) )
+dev.off()
