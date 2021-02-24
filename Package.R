1a94c7ffee054fc7eddb6eccdb0eea2853df01fc
Kaamelotte <48158127+Kaamelotte@users.noreply.github.com>
2021-02-24 11:24:41 +0100

    compilation et installation

--- Package.R
+++ Package.R
@@ -23,7 +23,7 @@ unlink(path, recursive=TRUE)
 
 my_description<-list("Title" = "Lockdown period Load forecasting",
                      "Version" ="0.0",
-                     "Authors@R"= c("person('Antoine', 'Caillebotte')","person('Pablo', 'Le Guern-Fiallo')"),
+                     "Authors@R"= "person('Antoine', 'Caillebotte', email = 'caillebotte.antoine@gmail.', role = c('aut', 'cre'))",
                      "Description" = "Lockdown period Load forecasting",
                      "License" = "GPL-3"
 )
@@ -54,8 +54,32 @@ usethis::use_data(data_test)
 
 
 
+#######################################################################################
+#____________________________Compilation et installation _____________________________#
+#######################################################################################
+#a mettre dans la console correspondant au projet du package !
+mydir <- "/Users/caill/Documents/GitHub/SIM202/Packages/"
+mypackage <- "Projet.SIM202"
+path <- file.path(mydir, mypackage)
+
+setwd(path)
+build(, quiet=T)
+install()
 
 
+
+#######################################################################################
+#____________________________Compilation et installation _____________________________#
+#######################################################################################
+#A mettre dans une console quelconque
+
+library(Projet.SIM202)
+library(xts)
+
+
+data("data_train")
+train_x_times_serie <- xts(data_train$Load,order.by = data_train$Date)
+plot(train_x_times_serie, ylab = "consomation en MW", xlab = "date", type = "l", col = "red")
 
 
 
